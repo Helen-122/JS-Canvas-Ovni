11fc24601137518b7b936202787dbcb948f7900e
Helen-122 <helene.mazourine@gmail.com>
2021-04-26 21:12:06 +0200

    ovni canvas

---
+++ script.js
@@ -0,0 +1,26 @@
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.moveTo(172,190);
+ctx.quadraticCurveTo(250,20,328,190);
+ctx.fillStyle='#F0FFFF';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(125,230);
+ctx.quadraticCurveTo(250,100,375,230);
+ctx.fillStyle='#B0C4DE';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(125,230);
+ctx.quadraticCurveTo(250,350,375,230);
+ctx.fillStyle='#B0C4DE';
+ctx.fill();
+ctx.closePath();
+
+
+
+
